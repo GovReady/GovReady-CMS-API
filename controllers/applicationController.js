17cd5bae876a16a02a8662a08ca2e48c89de6f81
Jeff Lyon <jeff@albatrossdigital.com>
2016-04-28 16:31:23 -0700

    Pull wordpress news from http://www.wpsecuritybloggers.com/category/wordfence/feed

--- controllers/applicationController.js
+++ controllers/applicationController.js
@@ -1,9 +1,9 @@
 // Load required packages
-var feedparser = require('feedparser');
-// WordPress RSS feed has faulty headers, so we need this
-// From http://stackoverflow.com/questions/36628420/nodejs-request-hpe-invalid-header-token
-process.binding('http_parser').HTTPParser = require('http-parser-js').HTTPParser;
 var request = require('request');
+var feed = require("feed-read");
+ 
+
+
 
 var helpers = require('../controllers/helpersController');
 
@@ -14,20 +14,19 @@ exports.getApplicationNews = function(req, res) {
 
   switch (req.params.app) {
     case 'wordpress':
-      var url = 'https://wordpress.org/news/feed/';
+      // Official WordPress RSS feed has faulty headers, so we're using an interesting feed from
+      // http://www.wpsecuritybloggers.com/category/wordpress
+      var url = 'http://www.wpsecuritybloggers.com/category/wordfence/feed';
+      //var url = 'http://www.wpsecuritybloggers.com/category/wp-white-security/feed';
       //var url = 'https://wordpress.org/news/category/security/feed/'; // @todo: change?
       break;
   }
-  console.log(url);
 
-  request(url, function (err, response, body) {
-    console.log(err);
+  feed(url, function(err, articles) {
+    if (err) throw err;
+
+    return res.status(200).json( articles ); 
   });
-  
-
-  feedparser.parseString(string)
-    .on('article', console.log);
-
 
 } // function
 
