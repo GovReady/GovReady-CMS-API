9b1b67bfaab943468e77b69d8157374bcedce602
Jeff Lyon <jeff@albatrossdigital.com>
2016-04-28 13:32:30 -0700

    Adding plugin controller, model. Moving vulnerabilites endpoint into pluginController

---
+++ models/pluginModel.js
@@ -0,0 +1,20 @@
+// Load required packages
+var mongoose = require('mongoose');
+
+// Define our beer schema
+var PluginSchema = new mongoose.Schema({
+  name: String,
+  fetched: Date,
+  application: String,
+  version: String,
+  release_date: String,
+  changelog_url: String,
+  vulnerabilities: Array,
+  phone: String,
+  responsibility: String,
+  lastConfirmed: String,
+  sub: String
+});
+
+// Export the Mongoose model
+module.exports = mongoose.model('Plugin', PluginSchema);
