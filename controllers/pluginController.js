71586506bb95b764c29d0e6f83a5c8ff26f13f42
Jeff Lyon <jeff@albatrossdigital.com>
2017-04-27 17:07:14 -0400

    Fxing fatal error in pluginController if item.version == null #67

--- controllers/pluginController.js
+++ controllers/pluginController.js
@@ -147,8 +147,8 @@ exports.calculateSiteVulnerabilities = function(site, cb) {
           item = item[0];
           plugin.vulnerabilities.forEach(function(vulnerability, j) {
             //item.version = '0.0.0'; // @todo: this is for testing only!!!
-            //console.log(vulnerability, vulnerability.fixed_in, item.version);
-            if ( vulnerability.fixed_in != null && cmp(vulnerability.fixed_in, item.version) > 0 ) {
+            console.log('FIXED IN', vulnerability.fixed_in, item.version);
+            if ( item.version != null && vulnerability.fixed_in != null && cmp(vulnerability.fixed_in, item.version) > 0 ) {
               vulnerabilities.push(vulnerability);
             }
           });
